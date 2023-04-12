3b1b5af15e68801547af48f241783e7a2920c36b
adrianUrbS <121135128+adrianUrbS@users.noreply.github.com>
2023-04-12 16:45:03 -0600

    ya funciona

--- main.js
+++ main.js
@@ -1,62 +1,80 @@
 // Crea la variable de referncia del lienzo usando fabric.Canvas()
-var canvas=new fabric.Canvas("myCanvas");
+var canvas = new fabric.Canvas("myCanvas");
 
 //Define el ancho inicial y el alto del cloque de imágenes 
-var medida=30;
+var medida = 30;
 
 //Define el inicio de las coordenadas  y Y del jugador 
-var X=50;
-var Y=50;
+var X = 50;
+var Y = 50;
 
 // Define una variable llamada player_object para almacenar la imagen del jugador
-var jugador="";
-var bloque="";
+var jugador = "";
+var bloque = "";
 // Agrega una función llamada player_update() para agregar la imagen del jugador 
-function dibujarJugador(){
-    fabric.Image.fromURL("player.png",function(img){
-        jugador=img;
+function dibujarJugador() {
+    fabric.Image.fromURL("player.png", function (img) {
+        jugador = img;
         jugador.scaleToWidth(150);
         jugador.scaleToHeight(150);
-        jugador.set({left:X,top:Y});
+        jugador.set({ left: X, top: Y });
         canvas.add(jugador);
     })
 }
-window.addEventListener("keydown" , teclaPulsar);
-function teclaPulsar(e){
-    tecla=e.keyCode;
-    if(tecla == "39" && X < 855 ){
+function dibujarBlock(imagen) {
+    fabric.Image.fromURL(imagen, function (img) {
+        bloque = img;
+        bloque.scaleToWidth(medida);
+        bloque.scaleToHeight(medida);
+        bloque.set({ left: X, top: Y });
+        canvas.add(bloque);
+    })
+}
+window.addEventListener("keydown", teclaPulsar);
+function teclaPulsar(e) {
+    tecla = e.keyCode;
+    if (tecla == "39" && X < 855) {
         X = X + medida;
         canvas.remove(jugador);
         dibujarJugador();
     }
-    if(tecla == "37" && X > 0){
-        X = X + medida;
+    if (tecla == "37" && X > 0) {
+        X = X - medida;
         canvas.remove(jugador);
-        dibujarjugador();
+        dibujarJugador();
     }
-    if(tecla == "40" && Y < 450){
+    if (tecla == "40" && Y < 450) {
+        Y = Y + medida;
+        canvas.remove(jugador);
+        dibujarJugador();
+    }
+    if (tecla == "38" && Y > 0) {
         Y = Y - medida;
         canvas.remove(jugador);
         dibujarJugador();
     }
-    if(tecla == "38" && Y > 0){
-        Y = Y - medida;
-        canvas.remove(jugador);
-        dibujarJugador();
-    }
-    if(e.shiftKey && tecla == "187"){
+    if (e.shiftKey && tecla == "107") {
         medida = medida + 10;
         document.getElementById("ancho").innerHTML = medida;
         document.getElementById("alto").innerHTML = medida;
     }
-    if(e.shiftKey && tecla == "189"){
+    if (e.shiftKey && tecla == "109") {
         medida = medida - 10;
         document.getElementById("ancho").innerHTML = medida;
         document.getElementById("alto").innerHTML = medida;
     }
-    if(tecla == "49"){
-      nuevoBloque("ground.png")
+    if (tecla == "49") {
+        dibujarBlock("ground.png")
     }
+    if (tecla == "50") {
+        dibujarBlock("roof.jpg")
+    }
+    if (tecla == "51") {
+        dibujarBlock("unique.png")
+    }
+    if(tecla == "52"){
+        dibujarBlock("Black_Cat.webp")
+      }
 }
 
 
